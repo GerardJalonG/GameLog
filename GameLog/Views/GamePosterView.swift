7f4936cdc8cbd9b43d979cb9e52bacba2101abfd
GerardJalon <gerard.jalon@gmail.com>
2025-12-05 16:33:42 +0100

    feat create Poster Game View

---
+++ GameLog/Views/GamePosterView.swift
@@ -0,0 +1,18 @@
+//
+//  GamePosterView.swift
+//  GameLog
+//
+//  Created by Gerard Jalon on 5/12/25.
+//
+
+import SwiftUI
+
+struct GamePosterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamePosterView()
+}

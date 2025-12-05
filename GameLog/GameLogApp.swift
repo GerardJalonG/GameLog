6e5d46756985914d23b77b64e98763c41c23bc10
GerardJalon <gerard.jalon@gmail.com>
2025-12-05 13:17:39 +0100

    Initial Commit

---
+++ GameLog/GameLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameLogApp.swift
+//  GameLog
+//
+//  Created by Gerard Jalon on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GameLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
